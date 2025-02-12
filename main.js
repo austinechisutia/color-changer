d4b97a14a5c4e31ab2bf64b3fff609977bf5ca98
austinechisutia <austineakhonya624@gmail.com>
2025-02-12 13:12:23 +0300

    created the html and css part

---
+++ main.js
@@ -0,0 +1,15 @@
+const colors = [
+    "#000000", // Black
+    "#FF0000", // Red
+    "#0000FF", // Blue
+    "#008000", // Green
+    "#FFFF00", // Yellow
+    "#800080", // Purple
+    "#00FFFF", // Cyan
+    "#FFA500", // Orange
+    "#FFC0CB", // Pink
+    "#A52A2A"  // Brown
+  ];
+  
+  console.log(colors[4]);
+  
